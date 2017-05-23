462d1f8dc0a992a153783f516e942440366b4cc0
Luki <Luki@DESKTOP-JBP1UAG>
2017-05-23 16:20:30 +0200

    Modules updated

---
+++ ApplicationRegistry/ApplicationRegistry-ServiceImpl/src/main/java/uni/iit/miskolc/lukacs6/ApplicationRegistry/ServiceImpl/App.java
@@ -0,0 +1,13 @@
+package uni.iit.miskolc.lukacs6.ApplicationRegistry.ServiceImpl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
