1f56e8a0054748c0c62ed2bd4b7879a69d5a7989
Luki <Luki@DESKTOP-JBP1UAG>
2017-05-25 13:56:13 +0200

    User Entity Converter added

---
+++ ApplicationRegistry/ApplicationRegistry-PersistImpl/src/main/java/uni/iit/miskolc/lukacs6/ApplicationRegistry/PersistImpl/converter/UserEntityConverter.java
@@ -0,0 +1,23 @@
+package uni.iit.miskolc.lukacs6.ApplicationRegistry.PersistImpl.converter;
+
+import uni.iit.miskolc.lukacs6.ApplicationRegistry.PersistImpl.entity.UserEntity;
+import uni.iit.miskolc.lukacs6.ApplicationRegistry.Model.User;
+
+public final class UserEntityConverter {
+
+	private UserEntityConverter() {
+	}
+
+	public static User convertUserEntityToModel(UserEntity userEntity) {
+		User user = new User();
+		user.setAge(userEntity.getAge());
+		user.setEmail(userEntity.getEmail());
+		user.setFirstname(userEntity.getFirstname());
+		user.setLastname(userEntity.getLastname());
+		user.setNeptuncode(userEntity.getNeptuncode());
+		user.setPassword(userEntity.getPassword());
+		user.setEnabled(userEntity.isEnabled());
+
+		return user;
+	}
+}
