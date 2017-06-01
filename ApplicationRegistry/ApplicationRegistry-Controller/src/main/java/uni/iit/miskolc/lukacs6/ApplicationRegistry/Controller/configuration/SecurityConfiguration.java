aef2e3a8eeef840f607235daf7649e300b11b7f4
Luki <Luki@DESKTOP-JBP1UAG>
2017-06-01 17:00:41 +0200

    + Security Conf(controller)

--- ApplicationRegistry/ApplicationRegistry-Controller/src/main/java/uni/iit/miskolc/lukacs6/ApplicationRegistry/Controller/configuration/SecurityConfiguration.java
+++ ApplicationRegistry/ApplicationRegistry-Controller/src/main/java/uni/iit/miskolc/lukacs6/ApplicationRegistry/Controller/configuration/SecurityConfiguration.java
@@ -7,8 +7,43 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
 
-public class SecurityConfiguration {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	DataSource dataSource;
 	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+			.usersByUsernameQuery("select username, password, enabled from users where username=?")
+			.authoritiesByUsernameQuery("select username, role from userroles where username=?");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/").permitAll()
+			.antMatchers("/student/**").access("hasRole('ROLE_STUDENT')")
+			.antMatchers("/teacher/**").access("hasRole('ROLE_TEACHER')")
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.and()
+				.formLogin().loginProcessingUrl("/login")
+				.usernameParameter("username")
+				.passwordParameter("password")
+			.and()
+				.logout().permitAll()
+			.and()
+				.csrf().disable();
+	}
 }
