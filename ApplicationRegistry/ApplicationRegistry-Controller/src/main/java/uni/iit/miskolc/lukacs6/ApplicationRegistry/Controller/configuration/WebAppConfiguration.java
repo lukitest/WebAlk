5de3db658d6533d8008c21a74830bedcb56b291d
Luki <Luki@DESKTOP-JBP1UAG>
2017-06-01 17:00:16 +0200

    WebApp Conf +

---
+++ ApplicationRegistry/ApplicationRegistry-Controller/src/main/java/uni/iit/miskolc/lukacs6/ApplicationRegistry/Controller/configuration/WebAppConfiguration.java
@@ -0,0 +1,23 @@
+package uni.iit.miskolc.lukacs6.ApplicationRegistry.Controller.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import uni.iit.miskolc.lukacs6.ApplicationRegistry.PersistImpl.*;
+import uni.iit.miskolc.lukacs6.ApplicationRegistry.ServiceImpl.configuration.ServiceApplicationContextConfig;;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("hu.iit.uni.miskolc.RequestRegistry.controller")
+@Import({SecurityConfiguration.class, DaoApplicationContextConfig.class, ServiceApplicationContextConfig.class})
+public class WebAppConfiguration extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+}
