88272114536c0cf95b975854938fcc406a35b632
Luki <Luki@DESKTOP-JBP1UAG>
2017-06-01 16:59:18 +0200

    ExceptionHandler@Controller

--- ApplicationRegistry/ApplicationRegistry-Controller/src/main/java/uni/iit/miskolc/lukacs6/ApplicationRegistry/Controller/ExceptionHandlerController.java
+++ ApplicationRegistry/ApplicationRegistry-Controller/src/main/java/uni/iit/miskolc/lukacs6/ApplicationRegistry/Controller/ExceptionHandlerController.java
@@ -1,5 +1,24 @@
 package uni.iit.miskolc.lukacs6.ApplicationRegistry.Controller;
 
-public class RequestController {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Exception.class)
+	@ResponseBody ErrorResponse
+	exception(Exception e) {
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setMessage("Internal server error");
+		errorResponse.setReason(null);
+		
+		e.printStackTrace();
+		return errorResponse;
+	}
 }
