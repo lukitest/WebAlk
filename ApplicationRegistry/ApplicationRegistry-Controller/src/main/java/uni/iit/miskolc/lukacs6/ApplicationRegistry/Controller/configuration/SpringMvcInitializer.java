2d188cac39fd41a568290d991d652624683a22ce
Luki <Luki@DESKTOP-JBP1UAG>
2017-06-01 16:59:57 +0200

    SpringMvcInitializer +

---
+++ ApplicationRegistry/ApplicationRegistry-Controller/src/main/java/uni/iit/miskolc/lukacs6/ApplicationRegistry/Controller/configuration/SpringMvcInitializer.java
@@ -0,0 +1,21 @@
+package uni.iit.miskolc.lukacs6.ApplicationRegistry.Controller.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { WebAppConfiguration.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+}
