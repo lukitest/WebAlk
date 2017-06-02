131f64546af044e0d45cb622af1546734b8db5e4
Luki <Luki@DESKTOP-JBP1UAG>
2017-06-02 08:00:55 +0200

    SQL Create DB added

---
+++ ApplicationRegistry/ApplicationRegistry-PersistImpl/src/main/resources/inidb/createDB.sql
@@ -0,0 +1,28 @@
+create database applicationregistry;
+use application registry;
+
+CREATE TABLE Users(
+	Neptuncode VARCHAR(200) PRIMARY KEY,
+	Password VARCHAR(200) NOT NULL,
+	Age INT NOT NULL,
+	FirstName VARCHAR(200) NOT NULL,
+	LastName VARCHAR(200) NOT NULL,
+	Email VARCHAR(200) NOT NULL,
+	IsTeacher BOOLEAN NOT NULL,
+	Enabled BOOLEAN NOT NULL
+);
+
+CREATE TABLE Templates(
+	Title VARCHAR(200) PRIMARY KEY,
+	Value VARCHAR(200) NOT NULL,
+	Recipient VARCHAR(200) NOT NULL
+);
+CREATE TABLE Requests(
+	ID INT PRIMARY KEY AUTO_INCREMENT,
+	UserID VARCHAR(200) NOT NULL,
+	TemplateTitle VARCHAR(200) NOT NULL,
+	Comment VARCHAR(200),
+	Status VARCHAR(200) NOT NULL,
+	FOREIGN KEY (UserID) REFERENCES Users(Neptuncode),
+	FOREIGN KEY (TemplateTitle) REFERENCES Templates(Title)
+);
